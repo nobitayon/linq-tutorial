731608e987c6e7e8f093309de877b7afac271fd2
yonathanm10 <yonathanm@cocoindonesia.com>
2024-06-08 11:24:38 +0700

    Video 2: Queries(Method Syntax and Query Syntax  select, where, join, left outer join)

---
+++ LINQExamples_1/Program.cs
@@ -0,0 +1,187 @@
+﻿namespace LINQExamples_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Employee> employeeList = Data.GetEmployes();
+            List<Department> departmentList= Data.GetDepartments();
+
+
+            // JOIN Query Syntax Example
+            //var results = from dept in departmentList
+            //              join emp in employeeList
+            //              on dept.Id equals emp.DepartmentId
+            //              select new
+            //              {
+            //                  FullName = emp.FirstName + " " + emp.LastName,
+            //                  AnnualSalary = emp.AnnualSalary,
+            //                  departmentName = dept.Longname
+            //              };
+            //foreach (var item in results)
+            //{
+            //    Console.WriteLine($"{item.FullName,-20} {item.AnnualSalary,10}\t{item.departmentName}");
+            //}
+
+            // GroupJoin Operator Example - Method Syntax
+            //var results = departmentList.GroupJoin(employeeList,
+            //    dep => dep.Id,
+            //    emp => emp.DepartmentId,
+            //    (dept, employeesGroup) => new
+            //    {
+            //        Employees = employeesGroup,
+            //        DepartmentName = dept.Longname
+            //    });
+            //foreach (var item in results)
+            //{
+            //    Console.WriteLine($"Department Name: {item.DepartmentName}");
+            //    foreach(var emp in item.Employees)
+            //    {
+            //        Console.WriteLine($"\t{emp.FirstName} {emp.LastName}");
+            //    }
+            //}
+
+            // GroupJoin Operator Example - Query Syntax
+            var results = from dept in departmentList
+                          join emp in employeeList
+                          on dept.Id equals emp.DepartmentId
+                          into employeeGroup
+                          select new
+                          {
+                              Employees = employeeGroup,
+                              DepartmentName = dept.Longname
+                          };
+
+            foreach (var item in results)
+            {
+                Console.WriteLine($"Department Name: {item.DepartmentName}");
+                foreach (var emp in item.Employees)
+                {
+                    Console.WriteLine($"\t{emp.FirstName} {emp.LastName}");
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+
+    public static class EnumerableCollectionExtensionMethods
+    {
+        public static IEnumerable<Employee> GetHighSalariedEmployees(this IEnumerable<Employee> employees)
+        {
+            foreach(Employee emp in employees)
+            {
+                Console.WriteLine($"Accessing employee: {emp.FirstName + " " + emp.LastName}");
+
+                if (emp.AnnualSalary >= 50000)
+                {
+                    yield return emp;
+                }
+            }
+
+        }
+    }
+
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public decimal AnnualSalary { get; set; }
+        public bool IsManager { get; set; }
+        public int DepartmentId { get; set; }
+
+    }
+
+    public class Department
+    {
+        public int Id { get; set; }
+        public string ShortName { get; set; }
+        public string Longname { get; set; }
+    }
+
+    public static class Data
+    {
+        public static List<Employee> GetEmployes()
+        {
+            List<Employee> employees = new List<Employee>();
+
+            Employee employee = new Employee
+            {
+                Id = 1,
+                FirstName = "Bob",
+                LastName = "Jones",
+                AnnualSalary = 60000.3m,
+                IsManager = true,
+                DepartmentId = 6,
+            };
+            employees.Add(employee);
+            employee = new Employee
+            {
+                Id = 2,
+                FirstName = "Sarah",
+                LastName = "Jameson",
+                AnnualSalary = 80000.1m,
+                IsManager = true,
+                DepartmentId = 2,
+            };
+            employees.Add(employee);
+            employee = new Employee
+            {
+                Id = 3,
+                FirstName = "Douglas",
+                LastName = "Roberts",
+                AnnualSalary = 40000.2m,
+                IsManager = false,
+                DepartmentId = 2,
+            };
+            employees.Add(employee);
+            employee = new Employee
+            {
+                Id = 4,
+                FirstName = "Jane",
+                LastName = "Stevens",
+                AnnualSalary = 30000.2m,
+                IsManager = false,
+                DepartmentId = 2,
+            };
+            employees.Add(employee);
+
+
+            return employees;
+
+        }
+
+        public static List<Department> GetDepartments()
+        {
+            List<Department> departments = new List<Department>();
+
+            Department department = new Department
+            {
+                Id = 1,
+                ShortName = "HR",
+                Longname = "Human Resources"
+            };
+            departments.Add(department);
+
+            department = new Department
+            {
+                Id = 2,
+                ShortName = "FN",
+                Longname = "Finance"
+            };
+            departments.Add(department);
+
+            department = new Department
+            {
+                Id = 3,
+                ShortName = "TE",
+                Longname = "Technology"
+            };
+            departments.Add(department);
+
+            return departments;
+        }
+
+    }
+}
--- readme.md
+++ readme.md
@@ -7,9 +7,101 @@ Link: https://www.youtube.com/playlist?list=PL4LFuHwItvKbzDl6MBp3XY0MrnALSfyub
 - `TCPData`
 - `TCPExtension`
 - `ThePretendCommpanyApplication`
-## Some points
+## Some summary
 - Class extension and Lambda expression
 - IEnumerable and IQueryable
 - Create class extension filter and try it to filter list of `Employee` and `Department`
 - Use already defined extension in `Linq`, `Where`
 - sql-like query syntax provided by `Linq`, but some query still need to use extension method provided by `Linq`
+
+# Part 2 - Queries
+## Project related to this
+- `LINQExamples_1`
+## Some summary
+- Method Syntax and Query Syntax
+- `Select` method syntax 
+    ```
+    var results = employeeList.Select(e => new
+            {
+                FullName = e.FirstName + " " + e.LastName,
+                AnnualSalary = e.AnnualSalary
+            });
+
+
+    // with chaining
+    var results = employeeList.Select(e => new
+            {
+                FullName = e.FirstName + " " + e.LastName,
+                AnnualSalary = e.AnnualSalary
+            }).Where(e => e.AnnualSalary >= 50000);
+
+
+    ```
+- `Select` Query Syntax
+    ```
+    var results = from emp in employeeList
+              select new
+              {
+                  FullName = emp.FirstName + " " + emp.LastName,
+                  AnnualSalary = emp.AnnualSalary
+              };
+
+    
+    // with where clause
+    var results = from emp in employeeList
+              where emp.AnnualSalary >=50000
+              select new
+              {
+                  FullName = emp.FirstName + " " + emp.LastName,
+                  AnnualSalary = emp.AnnualSalary
+              };
+    ```
+- Deffered and Immediate Execution<br>
+    Immediate execution with "To conversion method"  
+- `INNER JOIN` using operator `JOIN` Method Syntax
+    ```
+    var results = departmentList.Join(employeeList,
+    department => department.Id,
+    employee => employee.DepartmentId,
+    (department, Employee) => new
+    {
+        FullName = Employee.FirstName + " " + Employee.LastName,
+        AnnualSalary = Employee.AnnualSalary,
+        departmentName = department.Longname
+    });
+    ```
+- `INNER JOIN` using query syntax
+    ```
+    var results = from dept in departmentList
+              join emp in employeeList
+              on dept.Id equals emp.DepartmentId
+              select new
+              {
+                  FullName = emp.FirstName + " " + emp.LastName,
+                  AnnualSalary = emp.AnnualSalary,
+                  departmentName = dept.Longname
+              };
+    ```
+- `LEFT OUTER JOIN` using `GroupJoin` operator
+    ```
+    var results = departmentList.GroupJoin(employeeList,
+    dep => dep.Id,
+    emp => emp.DepartmentId,
+    (dept, employeesGroup) => new
+    {
+        Employees = employeesGroup,
+        DepartmentName = dept.Longname
+    });
+    ```
+- `LEFT OUTER JOIN` using `into` keyword and `join`
+    ```
+    var results = from dept in departmentList
+              join emp in employeeList
+              on dept.Id equals emp.DepartmentId
+              into employeeGroup
+              select new
+              {
+                  Employees = employeeGroup,
+                  DepartmentName = dept.Longname
+              };
+    ```
